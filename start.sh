acef1c50ca0cc1a2bb62406d658fe12aba7bed44
Pyari36 <88506599+Pyari36@users.noreply.github.com>
2022-08-14 12:24:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Pyari36/Thorappanbot.git /Thorappanbot
+  git clone https://github.com/Joelkb/DQ-The-File-Donor.git /DQ-The-File-Donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO https://github.com/Pyari36/Thorappanbot
 fi
-cd /Thorappanbot
+cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
