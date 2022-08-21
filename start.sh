03f790b5d0f5fa34857ae7a9f08c710ba73b806b
Pyari36 <88506599+Pyari36@users.noreply.github.com>
2022-08-21 12:52:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Pyari36/Thorappanbot.git /Thorappanbot
+  git clone https://github.com/Pyari36/Thorappanbot1.git /Thorappanbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO https://github.com/Pyari36/Thorappanbot
+  git clone $UPSTREAM_REPO https://github.com/Pyari36/Thorappanbot1
 fi
-cd /Thorappanbot
+cd /Thorappanbot1
 pip3 install -U -r requirements.txt
-echo "Starting Thorappanbot...."
+echo "Starting Thorappanbot1...."
 python3 bot.py
